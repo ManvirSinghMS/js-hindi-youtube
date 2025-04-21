3ea0e8cc94087bd67033640b999ee16add707b29
Manvir Singh <80736204+ManvirSinghMS@users.noreply.github.com>
2025-04-21 04:59:06 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 19241;
+let accountEmail = "Manvir@gmail.com";
+var accountPassword = "pk2103";
+
+
+console.table([accountId, accountEmail, accountPassword]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = true;
+console.log(typeof score); // string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
