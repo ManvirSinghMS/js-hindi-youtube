395125af5a0bdf2794a0956c84daeeb10ebc351a
Manvir Singh <80736204+ManvirSinghMS@users.noreply.github.com>
2025-04-23 11:35:35 +0000

    object part 2 in progress

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,10 @@
+ //const tinderUser = new Object(); // this is a singleton object 
+ const tinderUser = {} // this is a non singleton object   
+ tinderUser.name = "Manvir Singh",
+ tinderUser.age = 18,
+ tinderUser.isLoggedIn = false
+
+//  console.log(tinderUser) 
+
+
+
