1fb9f8818ce77c11fbc4c7128c7bf098d2b0f116
Manvir Singh <80736204+ManvirSinghMS@users.noreply.github.com>
2025-04-23 16:17:59 +0000

    object came to an end

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,5 +6,39 @@
 
 //  console.log(tinderUser) 
 
+const regularUser = {
+    email : "some@gmail.com",
+    fullName: {
+        userfullname:{
+            firstName: "Manvir",
+            lastName: "Singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullName) // dot notation
 
 
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+// const obj3 = {obj1, obj2} 
+
+const obj3 = Object.assign(obj1, obj2) // shallow copy
+
+console.log(obj3)
+
+const obj4 = {...obj1, ...obj2} // mostly used this method
+
+console.log(obj4) // nested object
+
+console.log(tinderUser.hasOwnProperty("name")) // check if the object has the property
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Manvir Singh",
+}
+
+const {courseInstructor:teacher}= course
+console.log(teacher) // destructuring object
