160dba97d1b5c1bf964665165b8ebfe7c442cee9
Manvir Singh <80736204+ManvirSinghMS@users.noreply.github.com>
2025-04-23 08:21:01 +0000

    array continues

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,9 @@
+const arr = [1, 2, 3, 4, 5];
+const Heros = ["Batman", "Ironman"]
+console.log(Heros);
+Heros.unshift("Superman");
+console.log(Heros);
+console.log(arr.includes(2))
+//.join() method concatenates all elements of an array into a string
+console.log(arr.slice(1,3));
+console.log(arr.splice(1,3));
