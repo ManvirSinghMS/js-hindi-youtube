94c9aec00cc36d677b6648f4ae6a09cc399632e2
Manvir Singh <80736204+ManvirSinghMS@users.noreply.github.com>
2025-04-26 10:44:07 +0000

    Iterations

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -9,5 +9,28 @@ for(const num of arr){
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`)
+    // console.log(`Each char is ${greet}`)
+}
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+console.log(map);
+
+for(const [key, value] of map){
+    console.log(`Key is ${key} and value is ${value}`);
+}
+
+const myObj = {
+    'game1': 'Cricket',
+    'game2': 'Football',
+}
+
+for(const [key,value] of myObj){
+    console.log(key, ':-', value)
 }
